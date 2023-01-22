fc15d85b381cdc74229b1b2b37de96c047220e31
Sara Chen <k890244@gmail.com>
2023-01-22 21:37:47 +0800

    feat: Add pages and browserRouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { useAuthContext } from "./hooks/useAuthContext";
+
+// styles
+import "./App.css";
+
+// pages and components
+import Home from "./pages/home/Home";
+import Navbar from "./components/Navbar";
+import Signup from "./pages/signup/Signup";
+import Login from "./pages/login/Login";
+import ProjectBoard from "./pages/projectBoard/ProjectBoard";
 
 function App() {
-  return <div></div>;
+  const { authIsReady, user } = useAuthContext();
+
+  return (
+    <div className="App">
+      {authIsReady && (
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/signup"
+              element={!user ? <Signup /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/login"
+              element={!user ? <Login /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/project"
+              element={user ? <ProjectBoard /> : <Navigate to="/login" />}
+            />
+          </Routes>
+        </BrowserRouter>
+      )}
+    </div>
+  );
 }
 
 export default App;

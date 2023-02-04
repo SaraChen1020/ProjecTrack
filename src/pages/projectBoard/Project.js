d573b3c130fe52f8fc2de71415792b53dc77aec2
Sara Chen <k890244@gmail.com>
2023-02-04 23:37:58 +0800

    feat: build project page to redirect to  first project board

---
+++ src/pages/projectBoard/Project.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { useEffect } from "react";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useCollection } from "../../hooks/useCollection";
+import { useNavigate } from "react-router-dom";
+
+export default function Project() {
+  const { user } = useAuthContext();
+  const { documents, error } = useCollection(user.uid);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (documents) {
+      navigate(`/project/${documents[0].id}`);
+    }
+  }, [documents]);
+
+  return <div>{/*...*/}</div>;
+}

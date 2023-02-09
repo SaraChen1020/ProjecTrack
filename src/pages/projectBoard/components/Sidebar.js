4fe98b066bc1f8697a28674831d9e2a8f05e0584
Sara Chen <k890244@gmail.com>
2023-02-09 19:32:58 +0800

    feat: add delete project feature

--- src/pages/projectBoard/components/Sidebar.js
+++ src/pages/projectBoard/components/Sidebar.js
@@ -1,47 +1,67 @@
 import React from "react";
-import { Link, useParams } from "react-router-dom";
+import { useState } from "react";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import { useAuthContext } from "../../../hooks/useAuthContext";
 import { useCollection } from "../../../hooks/useCollection";
 import { useAddProject } from "../../../hooks/useAddProject";
+import { useUpdateData } from "../../../hooks/useUpdateData";
 
 // styles & components
 import "./Sidebar.css";
 import { HiOutlineClipboardDocumentList } from "react-icons/hi2";
+import { BiTrash } from "react-icons/bi";
 
 export default function Sidebar() {
+  const navigate = useNavigate();
   const { user } = useAuthContext();
   const { documents, error } = useCollection("project", user.uid);
   const { addProject } = useAddProject();
+  const { deleteProject } = useUpdateData();
   const { id } = useParams();
   const paramsId = id;
 
   return (
     <>
-      {documents && (
-        <div className="sidebar">
-          <div className="main-title">
-            My Project
-            <div className="add-project" onClick={() => addProject(user)}>
-              ＋
-            </div>
+      <div className="sidebar">
+        <div className="main-title">
+          My Project
+          <div className="add-project" onClick={() => addProject(user)}>
+            ＋
           </div>
-          {documents.map((i) => {
-            const { id, title } = i;
-            return (
-              <Link to={`/project/${id}`} key={id}>
-                <div
-                  className={`project-link ${id == paramsId ? "active" : ""}`}
-                >
-                  <div className="doc-icon">
-                    <HiOutlineClipboardDocumentList />
-                  </div>
-                  <div>{title}</div>
-                </div>
-              </Link>
-            );
-          })}
         </div>
-      )}
+        {documents && (
+          <>
+            {documents.map((i) => {
+              const { id, title } = i;
+              return (
+                <div className="link-area" key={id}>
+                  <Link to={`/project/${id}`}>
+                    <div
+                      className={`project-link ${
+                        id == paramsId ? "active" : ""
+                      }`}
+                    >
+                      <div className="doc-icon">
+                        <HiOutlineClipboardDocumentList />
+                      </div>
+                      <div>{title}</div>
+                    </div>
+                  </Link>
+                  <div className={`del-icon ${id == paramsId ? "active" : ""}`}>
+                    <BiTrash
+                      className="trash-icon"
+                      onClick={() => {
+                        deleteProject(id);
+                        navigate("/project");
+                      }}
+                    />
+                  </div>
+                </div>
+              );
+            })}
+          </>
+        )}
+      </div>
     </>
   );
 }

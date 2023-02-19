6b9ad1e0eb96e16083de1567da373513131fdd02
Sara Chen <k890244@gmail.com>
2023-02-19 16:20:13 +0800

    style: change the hint word and style

--- src/pages/projectBoard/components/SelectUser.js
+++ src/pages/projectBoard/components/SelectUser.js
@@ -19,13 +19,13 @@ export default function SelectUser({ cardId, document, setUpdateCoworkers }) {
   const { addCardAssigner, deleteCardAssigner } = useUpdateData();
 
   useEffect(() => {
-    if (users && cardAssigner.length === 0) {
+    if (cardAssigner.length === 0) {
       //如果陣列為0，代表尚未指派過人員
       setEmpty(true);
-      setUnAssigner(users);
-    } else if (users && cardAssigner.length !== 0) {
+      setUnAssigner(document.coworkers);
+    } else if (cardAssigner.length !== 0) {
       //從所有會員中，將已被選取的會員移除列表
-      const result = users.filter((allUser) => {
+      const result = document.coworkers.filter((allUser) => {
         return !cardAssigner.find(
           (user) => user.uid === allUser.uid && user.name === allUser.name
         );
@@ -34,11 +34,7 @@ export default function SelectUser({ cardId, document, setUpdateCoworkers }) {
       setAlreadyAssign(cardAssigner);
       setUnAssigner(result);
     }
-  }, [cardAssigner, users]);
-
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
+  }, [cardAssigner, document.coworkers]);
 
   const handleAddAssign = (displayName, uid) => {
     const assignInfo = { displayName, uid };
@@ -51,91 +47,102 @@ export default function SelectUser({ cardId, document, setUpdateCoworkers }) {
   const handelDeleteAssign = (displayName, uid) => {
     const assignInfo = { displayName, uid };
     deleteCardAssigner(cardId, assignInfo);
-    // setUpdateCoworkers((prevData) => {
-    //   return prevData.filter((item) => item !== uid);
-    // });
   };
 
   return (
     <>
-      {users && (
-        <div className="select-user">
-          {!isSelect && (
-            <div
-              className="select-btn"
-              onClick={() => {
-                setShowMenu(true);
-                setIsSelect(true);
-              }}
-            >
-              {empty && "Select user..."}
-              {!empty &&
-                !showMenu &&
-                alreadyAssign.map((doc) => {
-                  const { displayName, uid } = doc;
-                  return (
-                    <div key={uid} className="name-content">
-                      <div className="name-icon">
-                        {displayName[0].toUpperCase()}
-                      </div>
-                      <div>{displayName}</div>
-                    </div>
-                  );
-                })}
-            </div>
-          )}
-          {isSelect && (
-            <div className="selecting-area">
-              {empty && "Select user..."}
-              {!empty &&
-                showMenu &&
-                alreadyAssign.map((doc) => {
-                  const { displayName, uid } = doc;
-                  return (
-                    <div key={uid} className="name-content">
-                      <div className="name-icon">
-                        {displayName[0].toUpperCase()}
-                      </div>
-                      <div>{displayName}</div>
-                      <div
-                        className="close-icon"
-                        onClick={() => {
-                          handelDeleteAssign(displayName, uid);
-                        }}
-                      >
-                        <IoIosClose />
-                      </div>
-                    </div>
-                  );
-                })}
-            </div>
-          )}
-
-          {showMenu && (
-            <ul className="user-menu">
-              {unAssigner.map((user) => {
-                const { displayName, uid } = user;
+      <div className="select-user">
+        {!isSelect && (
+          <div
+            className="select-btn"
+            onClick={() => {
+              setShowMenu(true);
+              setIsSelect(true);
+            }}
+          >
+            {empty &&
+              document.coworkers.length == 0 &&
+              "No user, please add user from sidebar"}
+            {empty && document.coworkers.length != 0 && "Select user..."}
+            {!empty &&
+              !showMenu &&
+              alreadyAssign.map((doc) => {
+                const { displayName, uid } = doc;
                 return (
-                  <li
-                    key={uid}
-                    className="option"
-                    onClick={() => {
-                      handleAddAssign(displayName, uid);
-                      setShowMenu(false);
-                      setIsSelect(false);
-                    }}
-                  >
+                  <div key={uid} className="name-content">
                     <div className="name-icon">
                       {displayName[0].toUpperCase()}
                     </div>
                     <div>{displayName}</div>
-                  </li>
+                  </div>
                 );
               })}
-            </ul>
-          )}
-        </div>
-      )}
+          </div>
+        )}
+        {isSelect && (
+          <div className="selecting-area">
+            {empty &&
+              document.coworkers.length == 0 &&
+              "No user, please add user from sidebar"}
+
+            {empty && document.coworkers.length != 0 && "Select user..."}
+            {!empty &&
+              showMenu &&
+              alreadyAssign.map((doc) => {
+                const { displayName, uid } = doc;
+                return (
+                  <div key={uid} className="name-content">
+                    <div className="name-icon">
+                      {displayName[0].toUpperCase()}
+                    </div>
+                    <div>{displayName}</div>
+                    <div
+                      className="close-icon"
+                      onClick={() => {
+                        handelDeleteAssign(displayName, uid);
+                      }}
+                    >
+                      <IoIosClose />
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        )}
+
+        {showMenu && document.coworkers.length != 0 && (
+          <ul className="user-menu">
+            {unAssigner.map((user) => {
+              const { displayName, uid } = user;
+              return (
+                <li
+                  key={uid}
+                  className="option"
+                  onClick={() => {
+                    handleAddAssign(displayName, uid);
+                  }}
+                >
+                  <div className="name-icon">
+                    {displayName[0].toUpperCase()}
+                  </div>
+                  <div>{displayName}</div>
+                </li>
+              );
+            })}
+            {unAssigner.length != 0 && (
+              <div
+                className="btn"
+                onClick={() => {
+                  setShowMenu(false);
+                  setIsSelect(false);
+                }}
+              >
+                Close
+              </div>
+            )}
+          </ul>
+        )}
+      </div>
     </>
   );
 }

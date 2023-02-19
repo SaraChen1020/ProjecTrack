67d2168b0cf948f293f0fb905901374412608fe0
Sara Chen <k890244@gmail.com>
2023-02-19 16:29:49 +0800

    feat: add new board and hide board features

--- src/pages/projectBoard/ProjectBoard.js
+++ src/pages/projectBoard/ProjectBoard.js
@@ -1,16 +1,20 @@
 import React from "react";
 import { useRef } from "react";
 import { useDocument } from "../../hooks/useDocument";
-import { useParams } from "react-router-dom";
+import { useUpdateData } from "../../hooks/useUpdateData";
 
 // styles & components
 import "./ProjectBoard.css";
 import Sidebar from "./components/Sidebar";
 import ProjectTitle from "./components/ProjectTitle";
 import Board from "./components/Board";
+import TitleTag from "./components/TitleTag";
+import { BiPlus } from "react-icons/bi";
 
 export default function ProjectBoard() {
   const { document, error } = useDocument();
+  const { addNewBoard } = useUpdateData();
+
   const draggingBoard = useRef();
   const dragOverBoard = useRef();
   const draggingItem = useRef();
@@ -22,28 +26,49 @@ export default function ProjectBoard() {
       <div className="project-board">
         {!document && <div className="error-message">{error}</div>}
         {document && (
-          <div className="content">
-            <ProjectTitle document={document} />
-            <div className="board-area">
-              {document.boards.ids.map((id, index) => {
-                return (
-                  <Board
-                    key={id}
-                    boardId={id}
-                    index={index}
-                    title={document.boards.byId[id].name}
-                    cardIds={document.boards.byId[id].cardIds}
-                    cardsById={document.cards.byId}
-                    document={document}
-                    draggingItem={draggingItem}
-                    draggingBoard={draggingBoard}
-                    dragOverItem={dragOverItem}
-                    dragOverBoard={dragOverBoard}
-                  />
-                );
-              })}
+          <>
+            <div className="content">
+              <ProjectTitle document={document} />
+              <div className="board-area">
+                {document.boards.ids.map((id, index) => {
+                  return (
+                    <Board
+                      key={id}
+                      boardId={id}
+                      index={index}
+                      title={document.boards.byId[id].name}
+                      show={document.boards.byId[id].show}
+                      cardIds={document.boards.byId[id].cardIds}
+                      cardsById={document.cards.byId}
+                      document={document}
+                      draggingItem={draggingItem}
+                      draggingBoard={draggingBoard}
+                      dragOverItem={dragOverItem}
+                      dragOverBoard={dragOverBoard}
+                    />
+                  );
+                })}
+                {document.boards.ids.length < 4 && (
+                  <BiPlus className="newBoard" onClick={() => addNewBoard()} />
+                )}
+                <div className="hide-area">
+                  <div className="hide-title">Hidden Board</div>
+
+                  {document.boards.ids.map((id, index) => {
+                    return (
+                      <TitleTag
+                        key={id}
+                        boardId={id}
+                        index={index}
+                        title={document.boards.byId[id].name}
+                        show={document.boards.byId[id].show}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
             </div>
-          </div>
+          </>
         )}
       </div>
     </div>
--- src/pages/projectBoard/components/Board.js
+++ src/pages/projectBoard/components/Board.js
@@ -7,11 +7,13 @@ import Card from "./Card";
 import { RxDotFilled } from "react-icons/rx";
 import { TiPencil } from "react-icons/ti";
 import { BsCheck } from "react-icons/bs";
+import { AiOutlineEyeInvisible } from "react-icons/ai";
 
 const Board = ({
   index,
   boardId,
   title,
+  show,
   cardIds,
   cardsById,
   document,
@@ -23,7 +25,7 @@ const Board = ({
   const [isEditingName, setIsEditingName] = useState(false);
   const [isAddingCard, setIsAddingCard] = useState(false);
   const [inputValue, setInputValue] = useState("");
-  const { changeBoardTitle, addNewCard } = useUpdateData();
+  const { changeBoardTitle, hideBoard, addNewCard } = useUpdateData();
   const [boardTitle, setBoardTitle] = useState(title);
 
   useEffect(() => {
@@ -43,94 +45,102 @@ const Board = ({
   };
 
   return (
-    <div className={`board board-${index}`}>
-      <span
-        className="status"
-        draggable
-        onDragEnter={(e) => handleDragEnter(e, boardId)}
-        onDragOver={(e) => e.preventDefault()}
-      >
-        <div className="status-title">
-          <RxDotFilled className="status-icon" />
-          {isEditingName ? (
-            <input
-              className="edit-board-title-input"
-              autoFocus
-              value={boardTitle}
-              onChange={(e) => setBoardTitle(e.target.value)}
-              onKeyPress={(e) => {
-                if (e.key === "Enter") {
+    <>
+      {show && (
+        <div className={`board board-${index}`}>
+          <span
+            className="status"
+            draggable
+            onDragEnter={(e) => handleDragEnter(e, boardId)}
+            onDragOver={(e) => e.preventDefault()}
+          >
+            <div className="status-title">
+              <RxDotFilled className="status-icon" />
+              {isEditingName ? (
+                <input
+                  className="edit-board-title-input"
+                  autoFocus
+                  value={boardTitle}
+                  onChange={(e) => setBoardTitle(e.target.value)}
+                  onKeyPress={(e) => {
+                    if (e.key === "Enter") {
+                      changeBoardTitle(boardTitle, boardId);
+                      setIsEditingName(false);
+                    }
+                  }}
+                />
+              ) : (
+                <div
+                  onClick={() => {
+                    setIsEditingName(true);
+                  }}
+                >
+                  {boardTitle}
+                </div>
+              )}
+            </div>
+            {isEditingName ? (
+              <BsCheck
+                className="check-icon"
+                onClick={() => {
                   changeBoardTitle(boardTitle, boardId);
                   setIsEditingName(false);
+                }}
+              />
+            ) : (
+              <TiPencil
+                className="edit-icon"
+                onClick={() => {
+                  setIsEditingName(true);
+                }}
+              />
+            )}
+            <AiOutlineEyeInvisible
+              className="closeEye"
+              onClick={() => hideBoard(boardId)}
+            />
+          </span>
+          {cardIds.map((i, index) => (
+            <Card
+              key={i}
+              id={i}
+              index={index}
+              boardId={boardId}
+              value={cardsById[i]}
+              document={document}
+              draggingItem={draggingItem}
+              draggingBoard={draggingBoard}
+              dragOverItem={dragOverItem}
+              dragOverBoard={dragOverBoard}
+            />
+          ))}
+
+          {isAddingCard ? (
+            <input
+              className="addCard-input"
+              autoFocus
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              onKeyPress={(e) => {
+                if (e.key === "Enter") {
+                  handleAddNewCard();
+                }
+              }}
+              onKeyDown={(e) => {
+                if (e.key === "Escape") {
+                  setInputValue("");
+                  setIsAddingCard(false);
                 }
               }}
             />
           ) : (
-            <div
-              onClick={() => {
-                setIsEditingName(true);
-              }}
-            >
-              {boardTitle}
+            <div className="add-card" onClick={() => setIsAddingCard(true)}>
+              +
             </div>
           )}
         </div>
-        {isEditingName ? (
-          <BsCheck
-            className="check-icon"
-            onClick={() => {
-              changeBoardTitle(boardTitle, boardId);
-              setIsEditingName(false);
-            }}
-          />
-        ) : (
-          <TiPencil
-            className="edit-icon"
-            onClick={() => {
-              setIsEditingName(true);
-            }}
-          />
-        )}
-      </span>
-      {cardIds.map((i, index) => (
-        <Card
-          key={i}
-          id={i}
-          index={index}
-          boardId={boardId}
-          value={cardsById[i]}
-          document={document}
-          draggingItem={draggingItem}
-          draggingBoard={draggingBoard}
-          dragOverItem={dragOverItem}
-          dragOverBoard={dragOverBoard}
-        />
-      ))}
-
-      {isAddingCard ? (
-        <input
-          className="addCard-input"
-          autoFocus
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          onKeyPress={(e) => {
-            if (e.key === "Enter") {
-              handleAddNewCard();
-            }
-          }}
-          onKeyDown={(e) => {
-            if (e.key === "Escape") {
-              setInputValue("");
-              setIsAddingCard(false);
-            }
-          }}
-        />
-      ) : (
-        <div className="add-card" onClick={() => setIsAddingCard(true)}>
-          +
-        </div>
       )}
-    </div>
+    </>
   );
 };
 

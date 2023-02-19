dea92fadcb84c268be36237f50f3fd9d8631941e
Sara Chen <k890244@gmail.com>
2023-02-19 20:05:27 +0800

    style: adjust the style and z-index

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,45 +12,48 @@ export default function Navbar() {
   const { user } = useAuthContext();
 
   return (
-    <nav className="header">
-      <div className="navbar">
-        <ul>
-          <li className="logo-icon">
-            <Link to="/">
-              <span className="title">
-                Projec
-                <img src={T} className="t-type" alt="T" />
-                rack
-              </span>
-            </Link>
-          </li>
+    <>
+      <nav className="header">
+        <div className="navbar">
+          <ul>
+            <li className="logo-icon">
+              <Link to="/">
+                <span className="title">
+                  Projec
+                  <img src={T} className="t-type" alt="T" />
+                  rack
+                </span>
+              </Link>
+            </li>
 
-          {!user && (
-            <>
-              <li>
-                <Link to="/login">
-                  <span className="sign-btn">登入</span>
-                </Link>
-              </li>
-              <li>
-                <Link to="/signup">
-                  <span className="sign-btn">註冊</span>
-                </Link>
-              </li>
-            </>
-          )}
-          {user && (
-            <>
-              <li className="greeting">Hello, {user.displayName}</li>
-              <li>
-                <button className="btn" onClick={logout}>
-                  登出
-                </button>
-              </li>
-            </>
-          )}
-        </ul>
-      </div>
-    </nav>
+            {!user && (
+              <>
+                <li>
+                  <Link to="/login">
+                    <span className="sign-btn">登入</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/signup">
+                    <span className="sign-btn">註冊</span>
+                  </Link>
+                </li>
+              </>
+            )}
+            {user && (
+              <>
+                <li className="greeting">Hello, {user.displayName}</li>
+                <li>
+                  <button className="btn" onClick={logout}>
+                    登出
+                  </button>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </nav>
+      <div className="navbar-placeholder" />
+    </>
   );
 }

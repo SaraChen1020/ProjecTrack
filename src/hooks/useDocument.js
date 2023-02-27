389ee8e9f62098913b6cd06ab4fc70deeadb3fc9
Sara Chen <k890244@gmail.com>
2023-02-27 18:03:39 +0800

    feat: add auth protect when access project

--- src/hooks/useDocument.js
+++ src/hooks/useDocument.js
@@ -2,13 +2,13 @@ import { projectFirestore } from "../utils/firebase";
 import { doc, onSnapshot } from "firebase/firestore";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { useAuthContext } from "./useAuthContext";
 
 export const useDocument = () => {
   const { docId } = useParams();
   const [document, setDocument] = useState("");
   const [error, setError] = useState(null);
-  const [coworker, setCoworker] = useState("");
-  const [empty, setEmpty] = useState(false);
+  const { user } = useAuthContext();
 
   useEffect(() => {
     const ref = doc(projectFirestore, "project", docId);
@@ -21,22 +21,23 @@ export const useDocument = () => {
         } else if (!doc.exists()) {
           console.log("No document");
           setError("Cannot find the project");
-        } else {
+        } else if (
+          //查詢專案中的owner或coworker是否有包含自己
+          doc.data().owner == user.uid ||
+          doc.data().coworkers.some((coworker) => coworker.uid == user.uid)
+        ) {
           setDocument({ id: doc.id, ...doc.data() });
           // update state
           setError(null);
+        } else {
+          //沒有權限查看這個project
+          setDocument("");
+          setError("Cannot access this project");
         }
-
-        // if (doc.exists() && doc.data().coworkers.length == 0) {
-        //   setEmpty(true);
-        // } else {
-        //   setEmpty(false);
-        //   setCoworker(doc.data().coworkers);
-        // }
       },
       (error) => {
         console.log(error);
-        setError("could not fetch the data");
+        setError("Could not fetch the data");
       }
     );
 
@@ -44,5 +45,5 @@ export const useDocument = () => {
     return () => unSub();
   }, [docId]);
 
-  return { document, error, coworker, empty };
+  return { document, error };
 };

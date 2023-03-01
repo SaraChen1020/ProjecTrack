bcca87f9425214a7b671e5abf62415a596b2331c
Sara Chen <k890244@gmail.com>
2023-03-01 16:24:53 +0800

    feat: add introduction for homepage

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,6 +4,12 @@ import { Link } from "react-router-dom";
 // styles
 import "./Home.css";
 import Line from "../../images/homepage_background.png";
+import Intro_1 from "../../images/intro-1.jpg";
+import DND_gif from "../../images/dnd.gif";
+import Intro_2 from "../../images/intro-2.jpg";
+import assign_gif from "../../images/assign.gif";
+import Intro_3 from "../../images/intro-3.jpg";
+import device from "../../images/device.jpg";
 
 export default function Home() {
   return (
@@ -12,14 +18,66 @@ export default function Home() {
         <img src={Line} className="background-pic" alt="line" />
         <div className="section">
           <div className="content">
-            <h2>Tracking your project progress.</h2>
-            <h3>追蹤並管理您的任務進度</h3>
+            <h1>Tracking your project progress.</h1>
+            <p>
+              ProjecTrack provides you an easy way to manage not only your own
+              project but also team project.
+            </p>
             <div className="start-btn">
-              <Link to="/project">Get Start Now!</Link>
+              <Link to="/project">Get Start Now !</Link>
             </div>
           </div>
           <div className="picture">
             <div className="pic"></div>
+          </div>
+        </div>
+      </div>
+      <div className="introduction">
+        <div className="intro">
+          <div className="intro-section">
+            <div className="intro-content">
+              <img src={Intro_1} alt="intro-1-pic" />
+              <h2>Drag and drop your tasks.</h2>
+              <p>Easy to classify and master the project progress.</p>
+            </div>
+            <div className="intro-pic-area">
+              <img src={DND_gif} alt="dnd-gif" />
+            </div>
+          </div>
+        </div>
+        <div className="intro">
+          <div className="intro-section">
+            <div className="intro-content">
+              <img src={Intro_2} alt="intro-2-pic" />
+              <h2>Assign project to others.</h2>
+              <p>Let the team manage the project together.</p>
+            </div>
+            <div className="intro-pic-area">
+              <img src={assign_gif} alt="assign-gif" />
+            </div>
+          </div>
+        </div>
+        <div className="intro">
+          <div className="intro-section">
+            <div className="intro-content">
+              <img src={Intro_3} alt="intro-3-pic" />
+              <h2>Protect your project privacy.</h2>
+              <p>Non-team members can't view project through the link.</p>
+            </div>
+            <div className="intro-pic-area">
+              <img src={assign_gif} alt="assign-gif" />
+            </div>
+          </div>
+        </div>
+        <div className="intro">
+          <div className="intro-section intro-section-4">
+            <div className="intro-content">
+              <h2>Easy to browse in various devices.</h2>
+              <p>You can easily browse content anytime on any device.</p>
+            </div>
+            <div className="intro-pic-area">
+              <img src={device} alt="devices" />
+            </div>
           </div>
         </div>
       </div>
